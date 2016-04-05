848d57da45fcf19217cc90e5f3e93166cb509b94
rli <1207676083@qq.com>
2016-04-05 17:11:29 +0800

    提交

--- myssm/src/main/java/com/sjtu/myssm/student/dao/impl/StudentDaoImpl.java
+++ myssm/src/main/java/com/sjtu/myssm/student/dao/impl/StudentDaoImpl.java
@@ -1,16 +1,17 @@
 package com.sjtu.myssm.student.dao.impl;
 
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.sjtu.myssm.student.dao.StudentDao;
 import com.sjtu.myssm.student.model.Student;
 
 @Repository("studentDao")
-public class StudentDaoImpl implements StudentDao {
+public class StudentDaoImpl extends SqlSessionDaoSupport implements StudentDao {
 
 	@Override
 	public Student getStudent(Long studentId) {
-		
+		this.getSqlSession();
 		return null;
 	}
 	
